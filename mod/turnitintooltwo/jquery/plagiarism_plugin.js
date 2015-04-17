564a7883837ecfd3a7d845e8a693b2518fcf56c7
Dheeraj <dheeraj.br2@gmail.com>
2015-04-17 12:01:12 +0530

    Dheeraj: push moodle

---
+++ mod/turnitintooltwo/jquery/plagiarism_plugin.js
@@ -0,0 +1,179 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+jQuery(document).ready(function($) {
+    $(document).on('mouseover', '.tii_links_container .tii_tooltip', function() {
+        $(this).tooltipster({ multiple: true });
+        return false;
+    });
+
+    $(document).on('click', '.pp_origreport_open', function() {
+        var classList = $(this).attr('class').replace(/\s+/,' ').split(' ');
+
+        for (var i = 0; i < classList.length; i++) {
+            if (classList[i].indexOf('origreport_') !== -1 && classList[i] != 'pp_origreport_open') {
+                var classStr = classList[i].split("_");
+                var url = "";
+                // URL must be stored in separate div on forums
+                if ($('.origreport_forum_launch_'+classStr[1]).length > 0) {
+                    url = $('.origreport_forum_launch_'+classStr[1]).html();
+                } else {
+                    url = $(this).attr("id");
+                }
+                openDV("origreport", classStr[1], classStr[2], url);
+            }
+        }
+    });
+
+    $(document).on('click', '.pp_grademark_open', function() {
+        var classList = $(this).attr('class').replace(/\s+/,' ').split(' ');
+
+        for (var i = 0; i < classList.length; i++) {
+            if (classList[i].indexOf('grademark_') !== -1 && classList[i] != 'pp_grademark_open') {
+                var classStr = classList[i].split("_");
+                var url = "";
+                // URL must be stored in separate div on forums
+                if ($('.grademark_forum_launch_'+classStr[1]).length > 0) {
+                    url = $('.grademark_forum_launch_'+classStr[1]).html();
+                } else {
+                    url = $(this).attr("id");
+                }
+                openDV("grademark", classStr[1], classStr[2], url);
+            }
+        }
+    });
+
+    // Open an iframe light box containing the Peermark Manager
+    if ($('.plagiarism_turnitin_peermark_manager_pp_launch').length > 0) {
+        $('.plagiarism_turnitin_peermark_manager_pp_launch').colorbox({
+            iframe:true, width:"802px", height:"772px", opacity: "0.7", className: "peermark_manager",
+            onLoad: function() { getLoadingGif(); },
+            onCleanup: function() { hideLoadingGif(); },
+            onClosed: function() {
+                refreshPPPeermarkAssignments();
+            }
+        });
+    }
+
+    // Open an iframe light box containing the Peermark reviews
+    $(document).on('click', '.peermark_reviews_pp_launch', function() {
+        $('.peermark_reviews_pp_launch').colorbox({
+            open:true,iframe:true, width:"802px", height:"772px", opacity: "0.7", className: "peermark_reviews",
+            onLoad: function() { getLoadingGif(); },
+            onCleanup: function() { hideLoadingGif(); }
+        });
+        return false;
+    });
+
+    // Open an iframe light box containing the Rubric View
+    $(document).on('click', '.rubric_view_pp_launch', function() {
+        $(this).colorbox({
+            open:true,iframe:true, width:"832px", height:"682px", opacity: "0.7", className: "rubric_view",
+            onLoad: function() { getLoadingGif(); },
+            onCleanup: function() { hideLoadingGif(); }
+        });
+        return false;
+    });
+
+    $(document).on('click', '.pp_turnitin_eula_link', function() {
+        $(this).colorbox({
+            open:true,iframe:true, width:"766px", height:"596px", opacity: "0.7", className: "eula_view", scrolling: "false",
+            onLoad: function() { getLoadingGif(); },
+            onComplete: function() {
+                $(window).on("message", function(ev) {
+                    var message = typeof ev.data === 'undefined' ? ev.originalEvent.data : ev.data;
+                    window.location.reload();
+                });
+            },
+            onCleanup: function() { hideLoadingGif(); }
+        });
+        return false;
+    });
+
+    // Launch the Turnitin EULA
+    if ($(".pp_turnitin_ula").length > 0) {
+        if ($('.editsubmissionform').length > 0) {
+            $('.editsubmissionform').hide();
+        }
+        if ($('.pp_turnitin_ula').siblings('.mform').length > 0) {
+            $('.pp_turnitin_ula').siblings('.mform').hide();
+        }
+    }
+
+    function getLoadingGif() {
+        var img = '<div class="loading_gif"></div>';
+        $('#cboxOverlay').after(img);
+        var top = $(window).scrollTop() + ($(window).height() / 2);
+        $('.loading_gif').css('top', top+'px');
+    }
+
+    function hideLoadingGif() {
+        $('.loading_gif').remove();
+    }
+
+    // Refresh Peermark assignments stored locally for this module
+    function refreshPPPeermarkAssignments() {
+        $.ajax({
+            type: "POST",
+            url: "../plagiarism/turnitin/ajax.php",
+            dataType: "json",
+            data: {action: "refresh_peermark_assignments", cmid: $('input[name="coursemodule"]').val(), sesskey: M.cfg.sesskey},
+            success: function(data) {}
+        });
+    }
+
+    // Open the DV in a new window in such a way as to not be blocked by popups.
+    function openDV(dvtype, submission_id, coursemoduleid, url) {
+        var url = url+'&viewcontext=box&cmd='+dvtype+'&submissionid='+submission_id+'&sesskey='+M.cfg.sesskey;
+
+        var dvWindow = window.open(url, 'dv_'+submission_id);
+        var width = $(window).width();
+        var height = $(window).height();
+        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+            dvWindow.document.write('<iframe id="dvWindow" name="dvWindow" width="'+width+'" height="'+height+'" sandbox="allow-popups allow-same-origin allow-top-navigation allow-forms allow-scripts"></iframe>');
+        } else {
+            dvWindow.document.write('<frameset><frame id="dvWindow" name="dvWindow"></frame></frameset>');
+        }
+        dvWindow.document.write('<script>document.body.style = \'margin: 0 0;\';</script'+'>'); 
+        dvWindow.document.getElementById('dvWindow').src = url;
+        dvWindow.document.close();
+        if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+            // beforeunload event does not work in Safari.
+            $(dvWindow).bind('unload', function() {
+                refreshScores(submission_id, coursemoduleid);
+            });
+        } else {
+            $(dvWindow).bind('beforeunload', function() {
+                refreshScores(submission_id, coursemoduleid);
+            });
+        }
+    }
+
+    function refreshScores(submission_id, coursemoduleid) {
+        $.ajax({
+            type: "POST",
+            url: "../../plagiarism/turnitin/ajax.php",
+            dataType: "json",
+            data: {action: "update_grade", submission: submission_id, cmid: coursemoduleid, sesskey: M.cfg.sesskey},
+            success: function(data) {
+                eval(data);
+                window.location = window.location;
+            }
+        });
+    }
+
+    // Update the DB value for EULA accepted
+    function userAgreementAccepted( user_id ){
+        $.ajax({
+            type: "POST",
+            url: "../../plagiarism/turnitin/ajax.php",
+            dataType: "json",
+            data: {action: 'acceptuseragreement', user_id: user_id},
+            success: function(data) {
+                window.location = window.location;
+            }
+        });
+    }
+});
---
+++ mod/turnitintooltwo/version.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   turnitintooltwo
+ * @copyright 2012 iParadigms LLC
+ */
+
+if (empty($plugin)) {
+	$plugin = new StdClass();
+}
+
+$plugin->version   = 2014012412;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012062500;
+$plugin->component = 'mod_turnitintooltwo';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release  = '2.3+';
+$plugin->cron      = 1800;        // Period for cron to check this module in seconds
